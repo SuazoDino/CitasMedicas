139c4e5029ad28847d348b0227db0646e803322b
DinZ-24 <dino.suazo.z@uni.pe>
2025-11-06 16:11:33 -0500

    la ia es una mierda

--- consultas/resources/js/app.js
+++ consultas/resources/js/app.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import '../css/auth.css' 
 
 axios.defaults.withCredentials = true
 /* axios.get('/sanctum/csrf-cookie') */
